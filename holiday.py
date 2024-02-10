5e70aca317076d2e685c4368665f8b1be5145990
Matthew Schneider <154096235+schneider1231@users.noreply.github.com>
2024-02-10 11:22:03 +0000

    Add files via upload

---
+++ holiday.py
@@ -0,0 +1,67 @@
+def plane_cost(city_flight):
+    """
+    Calculates the cost of the flight depending on the location
+    """
+    # holiday locations and daily cost
+    destinations = { 
+       "london":1000, 
+       "tokyo":800,
+       "new york":600,
+       "paris":250,
+    }
+
+    if city_flight in destinations:
+        cost = destinations[city_flight]  
+    else:
+        print("Incorrect Entry")     
+        cost = 0           
+    return cost   
+
+
+def hotel_cost(num_nights):
+    """
+    Calculates the total cost of the hotel with the rate of 70 per night
+    """
+    cost = 70 * num_nights
+    return cost      
+
+
+def car_rental(rental_days):
+    """
+    Calculates the total cost of the rental car with the rate of 50 per day
+    """
+    car_cost = 50 * rental_days
+    return car_cost
+
+
+def holiday_cost(hotel_cost,plane_cost,car_rental):
+    """
+    Calculates the total cost of the holiday by adding up all the costs
+    """
+    total = hotel_cost + plane_cost + car_rental
+    return total
+
+
+def main():
+    """
+    Gets an input from the user for the different locations, nights to be stayed 
+    and car rental days then calls the functions to calculate the total costs 
+    and returns the prices
+    """
+    print("Destinations london, tokyo, new york, paris")
+    city = input("Choose your destination: ").lower()
+    nights = int(input("How many nights you will be staying: "))
+    rent_days = int(input("How many days you will rent a car: "))
+
+    hotel_total = hotel_cost(nights)
+    flight_total = plane_cost(city)
+    car_total = car_rental(rent_days)
+    holiday_total = holiday_cost(hotel_total, flight_total, car_total)
+
+    print(f"\nThe total cost of staying in the hotel for {nights} nights is: £{hotel_total}")
+    print(f"The flight to {city} is: £{flight_total}")
+    print(f"The total cost  of the car rental for {rent_days} days is: £{car_total}")
+    print(f"the total cost for the holiday is £{holiday_total}")
+
+
+main()
